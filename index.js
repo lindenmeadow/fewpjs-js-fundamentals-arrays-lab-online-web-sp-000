e9f3fc2c4ecfe11b7d342340236f5252bf62a2c2
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-21 20:11:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ const append = ["Milo", "Otis", "Garfield"];
 const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
-
-
